e6463b2417b7876e1549b7ddeffbff92d23bcc3e
ccn333 <benjamin.cacan@gmail.com>
2022-08-04 02:30:43 +0200

    added spinner for loading

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+export const Spinner = () => {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+// from https://loading.io/css/
